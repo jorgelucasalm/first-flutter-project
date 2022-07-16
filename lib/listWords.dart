4bbc96aa4aeff8da56dd5c4399d3924328153ce7
Jorge Lucas <jorge.lucasalm@gmail.com>
2022-07-16 18:32:10 -0300

    feat: adiciona a troca de layout

---
+++ lib/listWords.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+enum ViewType { grid, list }
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
+  int _crossAxisCount = 2;
+  ViewType _viewType = ViewType.grid;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Startup Name Generator'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(_viewType == ViewType.list ? Icons.list : Icons.grid_view),
+        onPressed: () {
+          if (_viewType == ViewType.list) {
+            _crossAxisCount = 2;
+            _viewType = ViewType.grid;
+          } else {
+            _crossAxisCount = 1;
+            _viewType = ViewType.list;
+          }
+          setState(() {});
+        },
+      ),
+      body: Container(
+        child: _buildCard(),
+      ),
+    );
+  }
+
+  Widget _buildCard() {
+    // final largura = MediaQuery.of(context).size.width;
+
+    return GridView.builder(
+        padding: const EdgeInsets.all(16.0),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: _crossAxisCount,
+        ),
+        itemBuilder: (BuildContext _context, int i) {
+          if (i.isOdd && _viewType == ViewType.list) {
+            return const Divider();
+          }
+
+          final int index = i ~/ 2;
+
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRowCollumns(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRowCollumns(WordPair pair) {
+    // final largura = MediaQuery.of(context).size.width;
+    if (_viewType == ViewType.grid) {
+      return Card(
+        margin: EdgeInsets.all(16),
+        child: Center(
+          child: Text(
+            pair.asPascalCase,
+            style: _biggerFont,
+          ),
+        ),
+      );
+    } else {
+      return ListTile(
+        title: Text(
+          pair.asPascalCase,
+          style: _biggerFont,
+        ),
+      );
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:flutter_teste/listWords.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,88 +11,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: RandomWords(),
-    );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  const RandomWords({Key? key}) : super(key: key);
-
-  @override
-  _RandomWordsState createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.grid_view),
-        onPressed: () {},
-      ),
-      body: Container(
-        child: _buildCard(),
-      ),
-    );
-  }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (BuildContext _context, int i) {
-          if (i.isOdd) {
-            return const Divider();
-          }
-
-          final int index = i ~/ 2;
-
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildCard() {
-    // final largura = MediaQuery.of(context).size.width;
-
-    return GridView.count(
-      crossAxisCount: 2,
-      padding: const EdgeInsets.all(16.0),
-      childAspectRatio: 2,
-      children: <Widget>[
-        Card(
-          clipBehavior: Clip.antiAlias,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                child: Text(
-                  'Title',
-                  style: _biggerFont,
-                ),
-              ),
-            ],
-          ),
-        )
-      ],
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
     );
   }
 }
